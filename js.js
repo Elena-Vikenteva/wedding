e3740feab544ce2335836514d9ccd02e2bb0a91e
Elena <76530241+Elena-Vikenteva@users.noreply.github.com>
2022-02-12 14:37:48 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -18,3 +18,13 @@ window.onclick = function(event) {
         }
     }
 }
+
+
+////
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
